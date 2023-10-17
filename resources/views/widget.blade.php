36ac28efd5beab0cdcd2db279a8b1a1205a5d745
rerenatssupe <renats.supe@reatom.com>
2023-10-17 15:00:45 +0300

    widget html + includes

--- resources/views/widget.blade.php
+++ resources/views/widget.blade.php
@@ -1,15 +1,39 @@
-<div class="col-sm-6">
-                  <div class="card">
-                    <div class="card-body pb-0 mb-xxl-4 pb-1">
-                      <p class="mb-1 fs-3">Customers</p>
-                      <h4 class="fw-semibold fs-7">36,358</h4>
-                      <div class="d-flex align-items-center mb-3">
-                        <span class="me-2 rounded-circle bg-light-danger round-20 d-flex align-items-center justify-content-center">
-                          <i class="ti ti-arrow-down-right text-danger"></i>
-                        </span>
-                        <p class="text-dark fs-3 mb-0">+9%</p>
-                      </div>
-                    </div>
-                    <div id="customers"></div>
-                  </div>
-                </div>
+<link rel="stylesheet" href="{{ asset('dist/libs/owl.carousel/dist/assets/owl.carousel.min.css')}}">
+<link  id="themeColors"  rel="stylesheet" href="{{ asset('dist/css/style.min.css')}}" />
+
+
+ <!-- Project -->
+ <div class="col-lg-4">
+    <div class="row">
+    <!-- Customers -->
+    <div class="col-sm-6">
+    <div class="card">
+        <div class="card-body pb-0 mb-xxl-4 pb-1">
+            <p class="mb-1 fs-3">Customers</p>
+            <h4 class="fw-semibold fs-7">36,358</h4>
+            <div class="d-flex align-items-center mb-3">
+                <span class="me-2 rounded-circle bg-light-danger round-20 d-flex align-items-center justify-content-center">
+                    <i class="ti ti-arrow-down-right text-danger"></i>
+                </span>
+                <p class="text-dark fs-3 mb-0">+9%</p>
+            </div>
+        </div>
+        <div id="customers"></div>
+    </div>
+</div>
+
+<!--  Customizer -->
+    <!--  Import Js Files -->
+    <script src="{{ asset('dist/libs/jquery/dist/jquery.min.js') }}"></script>
+    <script src="{{ asset('dist/libs/simplebar/dist/simplebar.min.js') }}"></script>
+    <script src="{{ asset('dist/libs/bootstrap/dist/js/bootstrap.bundle.min.js') }}"></script>
+    <!--  core files -->
+    <script src="{{ asset('dist/js/app.min.js')}}"></script>
+    <script src="{{ asset('dist/js/app.init.js')}}"></script>
+    <script src="{{ asset('dist/js/app-style-switcher.js')}}"></script>
+    <script src="{{ asset('dist/js/sidebarmenu.js')}}"></script>
+    <script src="{{ asset('dist/js/custom.js')}}"></script>
+  
+    <script src="{{ asset('dist/libs/owl.carousel/dist/owl.carousel.min.js')}}"></script>
+    <script src="{{ asset('dist/libs/apexcharts/dist/apexcharts.min.js')}}"></script>
+    <script src="{{ asset('dist/js/dashboard.js')}}"></script>
